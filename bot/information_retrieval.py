69d0cd0fd3919d2c2c65e05052d5fd3f0341e485
Jabdiel_PR 🔥⚔ <Jabdiel2015@gmail.com>
2025-10-21 18:46:11 -0400

    added battlelog command to fetch the most recent battle from a user

--- bot/information_retrieval.py
+++ bot/information_retrieval.py
@@ -16,6 +16,56 @@ dummy_request = requests.get(f'https://api.clashroyale.com/v1/players/%23{dummy_
 dummy_data = dummy_request.json()
 print(dummy_request.status_code)
 
+def get_player_battlelog(player_tag):
+    CLASH_API_KEY = os.getenv('CLASH_API_KEY')
+
+    if not CLASH_API_KEY:
+        raise RuntimeError('CLASH_API_KEY is missing. Put it in your .env file.')
+    
+    request = requests.get(f'https://api.clashroyale.com/v1/players/%23{player_tag.upper()}/battlelog', headers={'Accept': 'application/json', 'Authorization': f'Bearer {CLASH_API_KEY}'})
+
+    data = request.json()
+
+    if request.status_code == 200:
+        return user_battlelog_card(data)
+    else:
+        return f"Error: {data.get('message', 'Unknown error occurred')}"
+    
+def user_battlelog_card(data):
+    latest_battle = data[0]
+    team = latest_battle.get('team', [])
+    opponent = latest_battle.get('opponent', [])
+
+    player1 = team[0]
+    player_name = player1.get('name', 'Unknown')
+    player_tag = player1.get('tag', 'N/A')
+    player_crowns = player1.get('crowns', 0)
+    player_clan = player1.get('clan', {}).get('name', 'No Clan')
+    player_cards_used, player_evos_used, num_evos_used = get_battlelog_cards(player1)
+
+
+    player2 = opponent[0]
+    opponent_name = player2.get('name', 'Unknown')
+    opponent_tag = player2.get('tag', 'N/A')
+    opponent_crowns = player2.get('crowns', 0)
+    opponent_clan = player2.get('clan', {}).get('name', 'No Clan')
+    opponent_cards_used, opponent_evos_used, opponent_num_evos_used = get_battlelog_cards(player2)
+
+
+    return f'Player Name 🫅: {player_name}\nPlayer Tag #️⃣: {player_tag}\nClan 🏰: {player_clan}\nTowers Destroyed 💥: {player_crowns}\nCards Used 🃏: {player_cards_used}\nEvos Used ♦️:({num_evos_used}): {player_evos_used}\n\nOpponent Name 🫅: {opponent_name}\nPlayer Tag #️⃣: {opponent_tag}\nClan 🏰: {opponent_clan}\nTowers Destroyed 💥: {opponent_crowns}\nCards Used 🃏: {opponent_cards_used}\nEvos Used ♦️:({opponent_num_evos_used}): {opponent_evos_used}'
+
+def get_battlelog_cards(user):
+    cards_used = []
+    evos_used = []
+    for card in user.get('cards', []):
+        cards_used.append(card.get('name', 'Unknown'))
+
+        if card.get('evolutionLevel'):
+            evos_used.append(card.get('name', 'Unknown'))
+    
+    return ', '.join(cards_used), ', '.join(evos_used), len(evos_used)
+
+
 def get_player_info(player_tag):
     CLASH_API_KEY = os.getenv('CLASH_API_KEY')
     
--- bot/main.py
+++ bot/main.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 from discord import app_commands
 from information_retrieval import get_player_info
+from information_retrieval import get_player_battlelog
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
@@ -24,12 +25,18 @@ async def on_ready():
 async def ping_slash(interaction: discord.Interaction):
     await interaction.response.send_message('Pong!')
 
-@bot.tree.command(name='user', description='Get Clash Royale player information using their tag id')
+@bot.tree.command(name='user_info', description='Get Clash Royale player information using their tag id')
 @app_commands.describe(user_id='The player tag id (don\'t include the #)')
-async def user_command(interaction: discord.Interaction, user_id: str):
+async def user_info_command(interaction: discord.Interaction, user_id: str):
     message = get_player_info(user_id)
-    await interaction.response.send_message(f'Player Info:\n{message}')
-    
+    await interaction.response.send_message(f'Player Info:\n\n{message}')
+
+@bot.tree.command(name='user_battlelog', description='Get the most recent battle of a player using their tag id')
+@app_commands.describe(user_id='The player tag id (don\'t include the #)')
+async def user_battlelog_command(interaction: discord.Interaction, user_id: str):
+    message = get_player_battlelog(user_id)
+    await interaction.response.send_message(f'Recent battle log:\n\n{message}')
+
 if __name__ == '__main__':
     if not DISCORD_TOKEN:
         raise RuntimeError('DISCORD_TOKEN is missing. Put it in your .env file.')
